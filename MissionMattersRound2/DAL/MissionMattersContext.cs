afed90d923b1b6e01daa2cdce1175f18d76ccaba
tkraupp435 <tkraupp435@gmail.com>
2018-12-11 18:52:35 -0700

    connecting to database

---
+++ MissionMattersRound2/DAL/MissionMattersContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using AzureSample.Models;
+
+namespace MissionMattersRound2.DAL
+{
+    public class MissionMattersContext : DbContext
+    {
+        public MissionMattersContext() : base ("MissionMattersContext")
+        {
+
+        }
+
+        public DbSet<Mission> Missions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet <MissionQuestion> MissionQuestions { get; set; }
+    }
+}
