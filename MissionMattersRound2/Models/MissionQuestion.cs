73459ba4bdf3755fe1fd9a090bb639eea1dd5af7
sierra.johnson756@gmail.com <sierra.johnson756@gmail.com>
2018-12-11 18:32:12 -0700

    User and MissionQuestion Model

---
+++ MissionMattersRound2/Models/MissionQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MissionMattersRound2.Models
+{
+    [Table("MissionQuestion")]
+    public class MissionQuestion
+    {
+        [Key]
+        int missionQuestionID;
+        int missionID;
+        int userID;
+        string question;
+        string answer;
+    }
+}
