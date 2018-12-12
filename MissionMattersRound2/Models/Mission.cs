8ea5fd8a55b5dc66997ce2180d08021e2e425cc0
sierra.johnson756@gmail.com <sierra.johnson756@gmail.com>
2018-12-11 18:51:07 -0700

    Mission Model

---
+++ MissionMattersRound2/Models/Mission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MissionMattersRound2.Models
+{
+    [Table("Mission")]
+    public class Mission
+    {
+        [Key]
+        public int missionID;
+        public string missionName;
+        public string missionPresidentName;
+        public string missionAddress;
+        public string language;
+        public string climate;
+        public string dominantReligion;
+        public string flagSymbol;
+    }
+}
