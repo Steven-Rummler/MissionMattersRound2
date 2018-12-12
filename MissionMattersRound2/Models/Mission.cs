9593e8e5c6c53f647285387444dec19af59f6b97
Tanner Garrett <tjabezgarrett@gmail.com>
2018-12-11 18:31:35 -0700

    Mission Model

---
+++ MissionMattersRound2/Models/Mission.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MissionMattersRound2.Models
+{
+    [Table("Mission")]
+    public class Mission
+    {
+        [Key]
+        public int missionID { get; set; }
+        public String missionName { get; set; }
+        public String missionPresidentName { get; set; }
+        public String missionAddress { get; set; }
+        public String language { get; set; }
+        public String climate { get; set; }
+        public String dominantReligion { get; set; }
+        public String flagSymbol { get; set; }
+
+    }
+}
+
